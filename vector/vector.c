28d70966f042ff422b16faea9480a2644abca07f
Ilya <i.anzhin@g.nsu.ru>
2023-04-12 15:58:10 +0700

    vector

---
+++ vector/vector.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct vector {
+    int *array;
+    int size;
+    int capacity;
+} vector;
+
+vector *vector_init(int size) {
+    vector *new = malloc(sizeof(vector));
+    if (size == 0) {
+        size = 10;
+    }
+    new->array = calloc(size, sizeof(int));
+    new->capacity = size;
+    new->size = 0;
+    return new;
+}
+
+void push_back(vector *v, int value) {
+    if (v->size < v->capacity) {
+        v->array[v->size] = value;
+        v->size++;
+    } else {
+        int new_size = v->capacity * 2;
+        int *tmp = realloc(v->array, sizeof(int) * new_size);
+        if (tmp != NULL) {
+            v->array = tmp;
+            v->capacity = new_size;
+            push_back(v, value);
+        } else {
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void resize(vector *v, int new_size) {
+    if (new_size < v->size) {
+        v->size = new_size;
+    } else {
+        int *tmp = realloc(v->array, new_size);
+        if (tmp == NULL) {
+            exit(EXIT_FAILURE);
+        }
+        for (int i = v->size; i < new_size; i++) {
+            tmp[i] = 0;
+        }
+        v->array = tmp;
+    }
+}
+
+void pop_back(vector *v) {
+    if (v->size == 0) {
+        exit(EXIT_FAILURE);
+    } else {
+        v->size--;
+    }
+}
+
+void clear_vector(vector *v) {
+    free(v->array);
+    free(v);
+}
+
+int get_size(vector *v) {
+    return v->size;
+}
+
+int get(vector *v, int ind) {
+    return v->array[ind];
+}
---
+++ vector/vector.h
@@ -0,0 +1,20 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct vector vector;
+
+vector *vector_init(int size);
+
+void push_back(vector *v, int value);
+
+void resize(vector *v, int new_size);
+
+void pop_back(vector *v);
+
+int get(vector *v, int ind);
+
+int get_size(vector *v);
+
+void clear_vector(vector *v);
+
+#endif //VECTOR_H
